fed07dcc9ab0755f273c688c3310728cafa6cd4f
Setiawan Joddy <setiawanjoddy@Joddys-MacBook-Pro.local>
2022-12-03 16:07:45 +0700

    Programmatic UIButton Implemented Successfully

--- Programmatic-UIButton/ViewController.swift
+++ Programmatic-UIButton/ViewController.swift
@@ -12,8 +12,33 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        let button = UIButton()
+        button.setTitle("Click Me", for: .normal)
+        button.backgroundColor = .systemBlue
+        view.addSubview(button)
+        button.frame = CGRect(x: 100, y: 100, width: 210, height: 50)
+        button.layer.cornerRadius = 15
+        button.addTarget(self, action: #selector(buttonTapped), for: .touchUpInside)
+
+        let label = UILabel(frame: CGRect(x: 100, y: 170, width: 210, height: 50))
+        label.textAlignment = .center
+        label.text = "Hello, Just Click It!"
+        view.addSubview(label)
+        
     }
-
-
+    
+    @objc func buttonTapped() {
+        if self.view.backgroundColor == .systemCyan {
+            self.view.backgroundColor = .white
+            print("Background Color is now White")
+            
+        }
+        else {
+            self.view.backgroundColor = .systemCyan
+            print("Background Color is now Cyan")
+        }
+        //print("Button Tapped")
+    }
+   
+    
 }
-
